a7127e513310cb3fec87c1861e740bcb6c09368a
Seeraullee Ayman <m.seeraulleeayman@gmail.com>
2024-02-16 23:49:57 +0400

    Add static file serving and API endpoints

--- server.js
+++ server.js
@@ -1,8 +1,31 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+let postData = [];
 
-app.listen(port, () => { console.log(`Server is running on port ${port}`); })
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+})
+
+app.get('/test-web-service', (req, res) => {
+  res.json(postData);
+})
+
+app.use(express.json());
+
+app.post('/test-web-service', (req, res) => {
+  postData.push(req.body);
+  res.json({ message: "POST request received", receivedData: req.body});
+});
+
+app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); })
