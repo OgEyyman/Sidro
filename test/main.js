1a69bc1d1edb192446001deefdf34cd347be3ec2
Seeraullee Ayman <m.seeraulleeayman@gmail.com>
2024-02-17 22:40:10 +0400

    Refactor toggle button functionality and add hover effect on SVG button

--- test/main.js
+++ test/main.js
@@ -1,8 +1,34 @@
 let toggleSwitch = document.getElementById("switch");
 let toggleContainer = document.querySelector(".content-utilities__toggle-feed");
 
-let toggleButtons = document.querySelectorAll(".content-utilities__feed-button");
+let toggleButtons = document.querySelectorAll(
+  ".content-utilities__feed-button"
+);
 let contentSections = document.querySelectorAll(".feed");
+
+let isClicked = false;
+
+document.getElementById("svgButton").addEventListener("mouseover", function () {
+  if (!isClicked) {
+    document.getElementById("svgImage").src = "hover-thumb.svg";
+  }
+});
+
+document.getElementById("svgButton").addEventListener("mouseout", function () {
+  if (!isClicked) {
+    document.getElementById("svgImage").src = "normal-thumb.svg";
+  }
+});
+
+document.getElementById("svgButton").addEventListener("click", function () {
+  if (isClicked) {
+    document.getElementById("svgImage").src = "normal-thumb.svg";
+    isClicked = false;
+  } else {
+    document.getElementById("svgImage").src = "liked-thumb.svg";
+    isClicked = true;
+  }
+});
 
 toggleButtons.forEach((button) => {
   button.addEventListener("click", () => {
@@ -22,7 +48,7 @@ toggleButtons.forEach((button) => {
     toggleContainer.style.boxShadow = "0 2px 3px 3px #569cd1";
     toggleContainer.style.transitionDuration = "0.3s";
 
-    setTimeout(function() {
+    setTimeout(function () {
       toggleContainer.style.boxShadow = "0 2px 1px 1px #569cd1bb";
     }, 300);
 
@@ -32,8 +58,7 @@ toggleButtons.forEach((button) => {
       section.classList.remove("feed--inactive");
       if (section.id === targetId) {
         section.classList.add("feed--active");
-      }
-      else {
+      } else {
         section.classList.add("feed--inactive");
       }
     });
