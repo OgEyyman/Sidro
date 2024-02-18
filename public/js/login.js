6197356b70c637c38ca626b6c0a8458a333a0bb8
Seeraullee Ayman <m.seeraulleeayman@gmail.com>
2024-02-18 14:35:18 +0400

    Refactor login and register pages

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,6 @@
 function renderLoginIcons() {
-  return `<div class="symbols">
+  return `
+  <div class="symbols">
   <img src="../assets/login/controller.png" alt="controller.png" />
   <img src="../assets/login/ps5.png" alt="ps5.png" />
   <img src="../assets/login/handheld_console.png" alt="handheld_console.png" />
@@ -24,48 +25,53 @@ function renderLoginIcons() {
   <img src="../assets/login/ps5.png" alt="ps5.png" />
   <img src="../assets/login/handheld_console.png" alt="handheld_console.png" /> 
   <img src="../assets/login/handheld.png" alt="handheld.png" />
-  </div>`;
-}
-
-export function animateLoginIcons() {
-  var images = document.getElementsByTagName("img");
-  for (var i = 0; i < images.length; i++) {
-    var duration = Math.random() * 15 + 1;
-    images[i].style.animationDuration = duration + "s";
-  }
+  </div>
+  `;
 }
 
 export function loadLoginPage() {
   return (
     renderLoginIcons() +
     `<div class="display-box">
-    <h1>Login</h1>  
-    <form>
-      <input type="text" class="display-box-username" name="username" placeholder="Username">
-      <input type="password" class="display-box-password" name="password" placeholder="Password">
+    <h1 class="display-box__header">Login</h1>  
+    <form class="display-box__form">
+      <input class="display-box__input" type="text" class="display-box-username" name="username" placeholder="Username">
+      <input class="display-box__input" type="password" class="display-box-password" name="password" placeholder="Password">
     </form>
-    <button>Login</button>
-    <p>New to Sidro™?</p>
-    <a href="#/register" id="registerLink">Register now!</a>      
-  </div>
-  `
+    <button class="display-box__button">Login</button>
+    <p class="display-box__redirect-text">New to Sidro™?</p>
+    <a class="display-box__redirect-link" href="#/register">Register now!</a>
+    </div>
+    `
   );
 }
 
 export function loadRegisterPage() {
   return (
     renderLoginIcons() +
-    `
-  <div class="display-box">
-    <h1>Register</h1>  
-    <form>
-      <input type="text" class="display-box-username" name="username" placeholder="Username">
-      <input type="password" class="display-box-password" name="password" placeholder="Password">
+    `<div class="display-box">
+    <h1 class="display-box__header">Register</h1>  
+    <form class="display-box__form">
+      <input class="display-box__input" type="text" class="display-box-username" name="username" placeholder="Username">
+      <input class="display-box__input" type="password" class="display-box-password" name="password" placeholder="Password">
     </form>
-    <button>Register</button>
-    <p>Already registered?</p>
-    <a href="#/login" id="registerLink">Login instead!</a>      
-  </div>
-  `
+    <button class="display-box__button">Register</button>
+    <p class="display-box__redirect-text">Already registered?</p>
+    <a class="display-box__redirect-link" href="#/login">Login instead!</a>
+    </div>
+    `
   );
+}
+
+export function initLogin() {
+  document
+    .querySelector(".display-box__button")
+    .addEventListener("click", function () {
+      window.location.hash = "#/home";
+    });
+  let images = document.getElementsByTagName("img");
+  for (let i = 0; i < images.length; i++) {
+    let duration = Math.random() * 15 + 1;
+    images[i].style.animationDuration = duration + "s";
+  }
 }
