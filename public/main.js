1782964574bcdeba07bd57d1c3afcd0b3b950bb8
Seeraullee Ayman <m.seeraulleeayman@gmail.com>
2024-02-18 15:20:26 +0400

    Add CSS loading and unloading functions, update navigation logic

--- public/main.js
+++ public/main.js
@@ -2,13 +2,22 @@ import { renderHeader, renderFooter } from "./js/layout.js";
 import { loadLoginPage, loadRegisterPage, initLogin } from "./js/login.js";
 import { loadHomePage, initHome } from "./js/home.js";
 
-document.addEventListener("DOMContentLoaded", () => {
-  navigateTo(window.location.hash);
-});
+function loadCSS(filename) {
+  let file = document.createElement("link");
+  file.setAttribute("rel", "stylesheet");
+  file.setAttribute("type", "text/css");
+  file.setAttribute("href", filename);
+  document.head.appendChild(file);
+}
 
-window.addEventListener("hashchange", () => {
-  navigateTo(window.location.hash);
-});
+function unloadCSS(filename) {
+  let links = document.getElementsByTagName("link");
+  for (let i = 0; i < links.length; i++) {
+    if (links[i].getAttribute("href") == filename) {
+      document.head.removeChild(links[i]);
+    }
+  }
+}
 
 function navigateTo(hash) {
   switch (hash) {
@@ -20,7 +29,7 @@ function navigateTo(hash) {
       loadPageContent(loadRegisterPage(), false);
       initLogin();
       break;
-      case "#/home":
+    case "#/home":
       loadPageContent(loadHomePage());
       initHome();
       break;
@@ -28,13 +37,13 @@ function navigateTo(hash) {
       loadPageContent(loadLoginPage(), false);
       initLogin();
       break;
-    }
+  }
 }
 
-function loadPageContent(content, displayHeader = true) {  
+function loadPageContent(content, displayHeader = true) {
   let header = document.getElementsByTagName("header")[0];
   let footer = document.getElementsByTagName("footer")[0];
-  
+
   if (header) {
     header.remove();
   }
@@ -44,10 +53,34 @@ function loadPageContent(content, displayHeader = true) {
   }
 
   if (displayHeader) {
-    document.body.insertAdjacentHTML('afterbegin', renderHeader());
-     }
+    document.body.insertAdjacentHTML("afterbegin", renderHeader());
+  }
 
   document.getElementsByClassName("content")[0].innerHTML = content;
 
-  document.body.insertAdjacentHTML('beforeend', renderFooter());
+  document.body.insertAdjacentHTML("beforeend", renderFooter());
 }
+
+loadCSS("./css/base.css");
+loadCSS("./css/login.css");
+
+document.addEventListener("DOMContentLoaded", () => {
+  navigateTo(window.location.hash);
+});
+
+window.addEventListener("hashchange", () => {
+  navigateTo(window.location.hash);
+});
+
+window.addEventListener("hashchange", function () {
+  switch (location.hash) {
+    case "#/login" || "#/register":
+      unloadCSS("./css/home.css");
+      loadCSS("./css/login.css");
+      break;
+    case "#/home":
+      unloadCSS("./css/login.css");
+      loadCSS("./css/home.css");
+      break;
+  }
+});
