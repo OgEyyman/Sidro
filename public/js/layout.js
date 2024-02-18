725043befc9eb2c9cd11a8cc9cb74fd51eaec9ae
Seeraullee Ayman <m.seeraulleeayman@gmail.com>
2024-02-18 14:35:14 +0400

    Refactor header layout

--- public/js/layout.js
+++ public/js/layout.js
@@ -1,15 +1,52 @@
 export function renderHeader() {
   return `
   <header>
-    <img src="" alt="menu icon" class="menu-icon" />
-    <img src="" alt="search icon" class="search-icon" />
-    <button>
-      <img src="" alt="toggle search" class="toggle-search" />
-      <p>Search...</p>
-    </button>
-    <img src="" alt="sidro logo" class="sidro-logo" />
-    <img src="" alt="account icon" class="account-icon" />
-  </header>
+  <nav>
+    <div class="nav-menu">
+      <div class="nav-menu__item item--menu">
+        <button class="nav-menu__button button--add-post">
+          <img
+            class="nav-menu__icon--friend-request"
+            src="../assets/common/friend_request.svg"
+            alt="friend-request"
+          />
+        </button>
+        <span class="nav-menu__text text--friend-request"
+          >Friend requests</span
+        >
+      </div>
+      <div class="nav-menu__item item--logo">
+        <button class="nav-menu__button button--add-post">
+          <img
+            class="nav-menu__icon--add-post"
+            src="../assets/common/add_post.svg"
+            alt="add-post"
+          />
+        </button>
+        <span class="nav-menu__text text--add-post">Add a post</span>
+      </div>
+      <div class="nav-menu__item item--profile">
+        <button class="nav-menu__button button--search">
+          <img class="nav-menu__icon" src="../assets/common/search.svg" alt="search" />
+        </button>
+        <span class="nav-menu__text text--search">Search</span>
+      </div>
+    </div>
+    <div class="nav-logo">
+      <img class="nav-logo__img" src="../assets/common/Slothlogo3.png" alt="sidro logo" />
+    </div>
+    <div class="nav-profile">
+      <button class="nav-profile__button">
+        <img
+          class="nav-profile__icon"
+          src="../assets/common/user_icon.svg"
+          alt="user icon"
+        />
+      </button>
+      <span class="nav-profile__text text--profile">Accounts</span>
+    </div>
+  </nav>
+</header>
   `;
 }
 
