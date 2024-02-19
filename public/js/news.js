53dd6f537aa52d4873d1b46015cb9e39b4c1f938
Seeraullee Ayman <m.seeraulleeayman@gmail.com>
2024-02-19 10:53:22 +0400

    Refactor news.js to include a loadNewsFeedPage function and an insertNewsFeedPage function

--- public/js/news.js
+++ public/js/news.js
@@ -1,5 +1,20 @@
-export function loadNewsFeedPage() {
-  return (`
-    
-  `);
+function loadNewsFeedPage() {
+  return `
+  <div class="news">
+    <div class="news__header">
+      <p class="news__header-text">Game update - Valorant</p>
+    </div>
+    <div class="news__title">
+      <p class="news__title-text">Valorant patch notes 1.01</p>
+      <a href="https://playvalorant.com/en-gb/news/game-updates/valorant-patch-notes-8-01/" 
+      target="_blank">
+      https://playvalorant.com/en-gb/news/game-updates/valorant-patch-notes-8-01/
+      </a>
+    </div>
+  </div>
+  `;
+}
+
+export function insertNewsFeedPage() {
+  document.getElementById("newsfeed").innerHTML = loadNewsFeedPage();
 }
