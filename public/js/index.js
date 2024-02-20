1bb206cf763977dcd13f77d6f7bb0afd934b25c1
Seeraullee Ayman <m.seeraulleeayman@gmail.com>
2024-02-20 12:24:28 +0400

    Add module exports for layout, login, homefeed, news, and profile

---
+++ public/js/index.js
@@ -0,0 +1,6 @@
+export { renderHeader, renderFooter, initHeader } from "./layout.js";
+export { loadLoginPage, loadRegisterPage, initLogin } from "./login.js";
+export { loadHomeFeedPage, initHome } from "./homefeed.js";
+export { insertNewsFeedPage } from "./news.js";
+export { loadProfilePage } from "./profile.js";
+
