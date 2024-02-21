8a19a5676d00f2fda36c8e5708dd6033b93f1010
Seeraullee Ayman <m.seeraulleeayman@gmail.com>
2024-02-21 19:33:09 +0400

    Update index.js with new page imports

--- public/components/index.js
+++ public/components/index.js
@@ -1,6 +1,10 @@
 export { renderHeader, renderFooter, initHeader } from "./layout.js";
 export { loadLoginPage, loadRegisterPage, initLogin } from "../pages/login.js";
 export { loadHomeFeedPage, initHome } from "../pages/homefeed.js";
-export { insertNewsFeedPage } from "../pages/news.js";
-export { loadAccountPage } from "../pages/profile.js";
-
+export { insertNewsFeedPage } from "../pages/newsfeed.js";
+export {
+  loadAccountPage,
+  loadOtherAccountPage1,
+  loadOtherAccountPage2,
+  initProfile,
+} from "../pages/profile.js";
