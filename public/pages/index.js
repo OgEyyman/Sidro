4ee8fbd988f2204c8072c9d99b41b34a7b7f2bf8
Seeraullee Ayman <m.seeraulleeayman@gmail.com>
2024-02-21 22:22:38 +0400

    Refactor component and page imports

---
+++ public/pages/index.js
@@ -0,0 +1,13 @@
+import * as layout from '../components/layout.js'
+import * as login from './login.js';
+import * as homefeed from './homefeed.js';
+import * as newsfeed from './newsfeed.js';
+import * as profile from './profile.js';
+
+export {
+  layout,
+  login,
+  homefeed,
+  newsfeed,
+  profile
+};
