c99d4700e900038d075f63d694436fbeec5456d7
Seeraullee Ayman <m.seeraulleeayman@gmail.com>
2024-02-21 19:32:20 +0400

    Refactor server.js file and connect to MongoDB

--- server.js
+++ server.js
@@ -1,31 +1,55 @@
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import express from "express";
+import { MongoClient, ServerApiVersion } from "mongodb";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-let postData = [];
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-})
-
-app.get('/test-web-service', (req, res) => {
-  res.json(postData);
-})
+app.use(express.static("public"));
 
 app.use(express.json());
 
-app.post('/test-web-service', (req, res) => {
-  postData.push(req.body);
-  res.json({ message: "POST request received", receivedData: req.body});
+const uri =
+  "mongodb+srv://ayman:bimbimbambam@coursework2.blgxzry.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: false,
+    deprecationErrors: false,
+  },
 });
 
-app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); })
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+run();
+
+app.get("/M00952409", (req, res) => {});
+
+app.post("/M00952409", async (req, res) => {
+  try {
+    const requestData = req.body; 
+    const db = client.db('Sidro');
+    const result = await db.collection('user').insertOne(requestData); 
+    if (result.insertedId) {
+        const insertedDocument = await db.collection('user').findOne({ _id: result.insertedId });
+        res.status(201).json(insertedDocument);
+    } else {
+        throw new Error('No document was inserted.');
+    }
+} catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Server Error' });
+}
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
