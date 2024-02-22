ba0b6a63dcd437f5e3455cdcae2e8d383c17c1a0
Seeraullee Ayman <m.seeraulleeayman@gmail.com>
2024-02-22 23:48:20 +0400

    implemented search functionality

--- public/components/layout.js
+++ public/components/layout.js
@@ -89,10 +89,29 @@ const renderHeader = /*HTML*/ `
       </div>
     </dialog>
     <dialog class="popup-search">
-      <label for="toggle">
-        <input type="checkbox" id="toggle">
-        <span class="slider"></span>
-      </label>
+      <div class="popup__header header--search">
+        <h1 class="popup__header__text">Search</h1>
+        <button class="popup__close close--search">
+          <img class="popup__close__icon" src="../assets/common/cross.svg"
+          alt="close button">
+        </button>
+      </div>
+      <div class="popup-search__container">
+        <div class="popup-search__search-bar">
+          <label class="popup-search__label" for="toggle">
+            <input class="popup-search__toggle" type="checkbox" id="toggle">
+            <span class="popup-search__slider"></span>
+          </label>
+          <input placeholder="Search users" type="text" 
+            class="popup-search__search-input" maxlength="38">
+          <button class="popup-search__button">
+            <img src="../assets/common/search.svg" alt="search icon" 
+              class="popup-search__search-icon">
+          </button>
+        </div>
+        <div class="popup-search__results">
+        </div>
+      </div>
     </dialog>
     <nav>
       <div class="nav-menu">
@@ -174,10 +193,12 @@ function initHeader() {
   const closePostButton = document.getElementsByClassName("close--post")[0];
   const addPostPopup = document.getElementsByClassName("popup__add-post")[0];
 
-  document.getElementsByClassName('post__button--attach')[0].
-  addEventListener('click', function() {
-    document.getElementById('imageUpload').click();
-  });
+  const openSearchButton = document.getElementsByClassName("button--search")[0];
+  const closeSearchButton = document.getElementsByClassName("close--search")[0];
+  const searchPopup = document.getElementsByClassName("popup-search")[0];
+  const toggle = document.getElementById('toggle');
+  const searchInput = document.getElementsByClassName('popup-search__search-input')[0];
+  const searchButton = document.getElementsByClassName('popup-search__button')[0];
 
   openFriendButton.addEventListener("click", () => {
     friendRequestPopup.showModal();
@@ -192,9 +213,134 @@ function initHeader() {
     addPostPopup.showModal();
     document.body.style.overflow = "hidden";
   });
+
   closePostButton.addEventListener("click", () => {
     addPostPopup.close();
     document.body.style.overflow = "auto";
+  });
+
+  document.getElementsByClassName('post__button--attach')[0].
+  addEventListener('click', function() {
+    document.getElementById('imageUpload').click();
+  });
+
+  openSearchButton.addEventListener("click", () => {
+    searchPopup.showModal();
+    document.body.style.overflow = "hidden";
+  });
+
+  closeSearchButton.addEventListener("click", () => {
+    searchPopup.close();
+    document.body.style.overflow = "auto";
+  });
+
+  toggle.addEventListener('change', function() {
+    if (this.checked) {
+      searchInput.placeholder = "Search posts";
+    } else {
+      searchInput.placeholder = "Search users";
+    }
+  });
+
+  searchButton.addEventListener('click', function() {
+    const placeholder = searchInput.getAttribute('placeholder');
+
+    if (placeholder === "Search posts") {
+      document.getElementsByClassName('popup-search__results')[0].innerHTML = /*HTML*/ `
+      <div class="post">
+        <div class="post__description">
+          <div class="post__user">
+            <img
+              class="post__user-profile-img"
+              src="../assets/home/user-icon-profile.svg"
+              alt="avatar"
+            />
+            <a class="post__username-link" href="#/John-Doe">
+              <span class="post__username">
+                John Doe
+              </span>
+            </a>
+          </div>
+          <div class="post__content">
+            <p class="post__content-description">
+              porttitor viverra et, dapibus sit amet hulla.porttitor viverra
+              et, dapibus sit amet hulla.porttitor viverra et, dapibus sit
+              amet hulla.porttitor viverra et, dapibus sit amet
+              hulla.porttitor viverra et, dapibus sit amet hulla. porttitor
+              viverra et, dapibus sit amet hulla.porttitor viverra et,
+              dapibus sit amet hulla.porttitor viverra et, dapibus sit amet
+              hulla.porttitor viverra et, dapibus sit amet hulla.porttitor
+              viverra et, dapibus sit amet hulla. porttitor viverra et,
+              dapibus sit amet hulla.porttitor viverra et, dapibus sit amet
+              hulla.porttitor viverra et, dapibus sit amet hulla.porttitor
+              viverra et, dapibus sit amet hulla.porttitor viverra et,
+              dapibus sit amet hulla. porttitor viverra et, dapibus sit amet
+              hulla.porttitor viverra et, dapibus sit amet hulla.porttitor
+              viverra et, dapibus sit amet hulla.porttitor viverra et,
+              dapibus sit amet, viverra et, dapibus sit amet hulla.porttitor 
+              viverra et, dapibus sit amet hulla. porttitor viverra et, 
+              dapibus sit amet hulla.porttitor viverra et, dapibus sit amet 
+              hulla.porttitor viverra et, dapibus sit amet hulla.porttitor 
+              viverra et, dapibus sit amet
+            </p>
+          </div>
+          <div class="post__details">
+            <div class="post__interactions">
+              <button id="svgButton" class="post__button-like">
+                <img
+                  id="svgImage"
+                  src="../assets/home/normal-thumb.svg"
+                  alt="like button"
+                />
+              </button>
+              <span class="post__button-like-count">15</span>
+              <img
+                class="post__button-comment"
+                src="../assets/home/comment.svg"
+                alt="comment button"
+              />
+              <span class="post__button-comment-count">2</span>
+            </div>
+            <time class="post__time" datetime="2023-10-01">10/01/2023</time>
+          </div>
+        </div>
+      </div>
+    `;
+    } else {
+      document.getElementsByClassName('popup-search__results')[0].innerHTML = /*HTML*/ `
+      <div class="popup__request-container">
+        <button class="popup__add-friend__button">
+          <img src="../assets/common/add-friend.svg" alt="add friend" 
+          class="popup__add-friend__icon">
+        </button>
+        <img src="../assets/common/account_icon.svg" alt="user icon" 
+        class="popup__user-icon">
+        <a href="#/bimbimbambam" class="popup__request__author">
+          bimbimbambam
+        </a>
+      </div>
+      <div class="popup__request-container">
+        <button class="popup__add-friend__button">
+          <img src="../assets/common/add-friend.svg" alt="add friend" 
+          class="popup__add-friend__icon">
+        </button>
+        <img src="../assets/common/account_icon.svg" alt="user icon" 
+        class="popup__user-icon">
+        <a href="#/John-Doe" class="popup__request__author">John Doe</a>
+      </div>
+      <div class="popup__request-container">
+        <button class="popup__add-friend__button">
+          <img src="../assets/common/add-friend.svg" alt="add friend"
+          class="popup__add-friend__icon">
+        </button>
+        <img src="../assets/common/account_icon.svg" alt="user icon" 
+        class="popup__user-icon">
+        <a href="#/gamerhafsah26" class="popup__request__author">
+          gamerhafsah26
+        </a>
+      </div>
+      `
+    }
   });
 }
 
