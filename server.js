a1226a9f39b2764f616df5f68efa511be5db7a9c
Seeraullee Ayman <m.seeraulleeayman@gmail.com>
2024-03-12 16:59:16 +0400

    Refactor server.js to use mongoose and add middleware

--- server.js
+++ server.js
@@ -1,33 +1,34 @@
 import express from "express";
-import { MongoClient, ServerApiVersion } from "mongodb";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import expressSession from "express-session";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.static("public"));
-
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(
+  expressSession({
+    secret: "CST2120_CW2",
+    cookie: { maxAge: 36288000 },
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 const uri =
   "mongodb+srv://ayman:bimbimbambam@coursework2.blgxzry.mongodb.net/?retryWrites=true&w=majority";
 
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: false,
-    deprecationErrors: true,
-  },
-});
-
 app.get("/M00952409/user-data", async (req, res) => {
   try {
-    const db = client.db('Sidro');
-    const collection = db.collection('users');
+    const db = client.db("Sidro");
+    const collection = db.collection("users");
     const result = await collection.find({}).toArray();
     res.status(200).json(result);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'An error occurred while fetching data from the database.' });
+    res.status(500).json({ message: "An error occurred while fetching data from the database." });
   }
 });
 
@@ -35,34 +36,42 @@ app.post("/M00952409/user-data", async (req, res) => {
   const user = {
     name: req.body.name,
     studentID: req.body.studentID,
-    email: req.body.email
+    email: req.body.email,
   };
   try {
-    const document = await client.db('Sidro').collection('users').insertOne(user);
+    const document = await client.db("Sidro").collection("users").insertOne(user);
     if (document.insertedId) {
       res.status(201).json(document);
     }
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: 'Server Error' });
+    res.status(500).json({ message: "Server Error" });
   }
 });
 
 /**
- * Runs the application.
- * @returns {Promise<void>} A promise that resolves when the application finishes running.
+ * Establishes a connection to the mongoose database.
+ * @async
+ * @function connection_to_db
+ * @throws {Error} If there is an error connecting to the database.
  */
-async function run() {
+async function connection_to_db() {
   try {
-    await client.connect();
-    console.log("Successfully connected to MongoDB!");
+    await mongoose.connect(uri);
+
+    bootServer();
   } catch (err) {
-    console.error(err);
+    console.log(`Error: ${err}`);
   }
 }
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+/**
+ * Boots up the server and starts listening on the specified port.
+ */
+function bootServer() {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
 
-run();
+connection_to_db();
