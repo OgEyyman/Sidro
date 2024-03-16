5fb100a59d86245e904a27c574dbc9d1a424eaab
Seeraullee Ayman <m.seeraulleeayman@gmail.com>
2024-03-16 23:58:54 +0400

    Refactor module imports and add validation module

---
+++ public/views/index.js
@@ -0,0 +1,12 @@
+import * as layout from "./layout.js";
+import * as login from "./login.js";
+import * as homefeed from "./homefeed.js";
+import * as newsfeed from "./newsfeed.js";
+import * as profile from "./profile.js";
+import * as validate from "../utils/validation.js";
+
+/**
+ * This module exports the layout, login, homefeed, newsfeed, and profile functions.
+ * @module index
+ */
+export { layout, login, homefeed, newsfeed, profile, validate };
