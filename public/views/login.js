062c9435c8d99f0d020c1ab0378112bf204d7607
Seeraullee Ayman <m.seeraulleeayman@gmail.com>
2024-03-16 23:59:55 +0400

    Add login and registration functionality

--- public/views/login.js
+++ public/views/login.js
@@ -1,3 +1,5 @@
+import { validateUsername, validatePassword } from "../utils/validation.js";
+
 const renderLoginIcons = /*HTML*/ `
   <!-- Symbols container -->
   <div class="symbols">
@@ -30,7 +32,8 @@ const renderLoginIcons = /*HTML*/ `
   `;
 
 const loadLoginPage =
-  renderLoginIcons + /*HTML*/ `
+  renderLoginIcons +
+  /*HTML*/ `
   <!-- Login display box -->
   <div class="display-box">
     <!-- Header -->
@@ -40,6 +43,7 @@ const loadLoginPage =
       <!-- Username input -->
       <input class="display-box__input input--username" type="text" maxlength="21"
         name="username" placeholder="Username">
+      <!-- Password input -->
       <div class="display-box__password-input">
         <!-- Password input with show password button -->
         <input class="display-box__input input--password" type="password" maxlength="19"
@@ -50,8 +54,9 @@ const loadLoginPage =
         </button>
       </div>
     </form>
+    <span id="error-message"></span>
     <!-- Login button -->
-    <button class="display-box__submit">Login</button>
+    <button class="display-box__submit submit--login">Login</button>
     <!-- Registration redirect -->
     <p class="display-box__redirect-text">New to Sidro™?</p>
     <a class="display-box__redirect-link" href="#/register">Register now!</a>
@@ -59,13 +64,15 @@ const loadLoginPage =
   `;
 
 const loadRegisterPage =
-  renderLoginIcons  + /*HTML*/
-  `<div class="display-box">
+  renderLoginIcons +
+  /*HTML*/ `
+  <div class="display-box">
     <h1 class="display-box__header">Register</h1>
     <!-- Registration form -->
     <form class="display-box__form">
       <input class="display-box__input input--username" type="text" maxlength="15"
       name="username" placeholder="Username">
+      <span id="error-message--username"></span>
       <div class="display-box__password-input">
         <input class="display-box__input input--password" type="password" maxlength="19"
           name="password" placeholder="Password">
@@ -73,9 +80,10 @@ const loadRegisterPage =
           <img class="display-box__show-password__image" src="../assets/login/show-button.svg"
           alt="show password">
         </button>
-      </div>
+        </div>
+      <span id="error-message--password"></span>
     </form>
-    <button class="display-box__submit">Register</button>
+    <button class="display-box__submit submit--register">Register</button>
     <!-- Login redirect -->
     <p class="display-box__redirect-text">Already registered?</p>
     <a class="display-box__redirect-link" href="#/login">Login instead!</a>
@@ -83,11 +91,22 @@ const loadRegisterPage =
   `;
 
 /**
- * Initializes the login functionality.
+ * Initialises the login functionality.
  */
 function initLogin() {
-  const loginButton = document.querySelector(".display-box__submit");
+  const submitLogin = document.querySelector(".submit--login");
+  const submitSignup = document.querySelector(".submit--register");
+
+  if (submitLogin) {
+    submitLogin.addEventListener("click", login);
+  }
+
+  if (submitSignup) {
+    submitSignup.addEventListener("click", register);
+  }
+
   const images = document.getElementsByTagName("img");
+
   for (let i = 0; i < images.length; i++) {
     const duration = Math.random() * 15 + 1;
     images[i].style.animationDuration = duration + "s";
@@ -104,17 +123,98 @@ function initLogin() {
       passwordInput.type = "password";
     }
   });
+}
 
-  document.querySelector(".display-box__submit").addEventListener("click", function () {
-    window.location.hash = "#/home";
-  });
+/**
+ * Performs the login operation.
+ * @async
+ * @function login
+ */
+async function login() {
+  const errorBox = document.getElementById("error-message");
 
-  loginButton.addEventListener("click", () => {
-    const username = document.querySelector(".input--username").value;
-    const password = document.querySelector(".input--password").value;
+  const username = document.querySelector(".input--username").value;
+  const password = document.querySelector(".input--password").value;
 
+  if (username.length != 0 && password.length != 0) {
+    try {
+      const res = await fetch("/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username: username, password: password }),
+      });
 
-  })
+      const resData = await res.json();
+
+      if (res.status === 200) {
+        window.location.hash = "#/home";
+      } else if (res.status === 401) {
+        errorBox.textContent = resData.message;
+      } else if (res.status === 400) {
+        errorBox.textContent = resData.message;
+      }
+    } catch (error) { console.error("Error:", error);}
+  } else {
+    errorBox.textContent = "Please fill in all fields.";
+  }
+}
+
+/**
+ * Registers a new user with unique username.
+ * @async
+ * @function register
+ */
+async function register() {
+  const errorBoxUser = document.getElementById("error-message--username");
+  const errorBoxPassword = document.getElementById("error-message--password");
+  errorBoxUser.textContent = "";
+  errorBoxPassword.textContent = "";
+
+  const username = document.querySelector(".input--username").value;
+  const password = document.querySelector(".input--password").value;
+
+  const validUsername = validateUsername(username);
+  const validPassword = validatePassword(password);
+
+  if (validUsername.isValid && validPassword.isValid) {
+    try {
+      const res = await fetch("/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username: username, password: password }),
+      });
+
+      const resData = await res.json();
+
+      if (res.status === 201) {
+        window.location.hash = "#/home";
+      } else if (res.status === 409) {
+        errorBoxUser.textContent = resData.message;
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  } else {
+    if (!validUsername.isValid) {
+      errorBoxUser.textContent = validUsername.message;
+    }
+
+    if (!validPassword.isValid) {
+      errorBoxPassword.textContent = validPassword.message;
+    }
+  }
+
+  if (username.length === 0) {
+    errorBoxUser.textContent = "Please fill in your username";
+  }
+
+  if (password.length === 0) {
+    errorBoxPassword.textContent = "Please fill in your password";
+  }
 }
 
 export { loadLoginPage, loadRegisterPage, initLogin };
