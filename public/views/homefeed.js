aed0143f85c03575efe1975374a6865955cb92cc
Seeraullee Ayman <m.seeraulleeayman@gmail.com>
2024-03-18 22:59:25 +0400

    Fix bug in login functionality

--- public/views/homefeed.js
+++ public/views/homefeed.js
@@ -44,168 +44,97 @@ const loadHomeFeedPage = /*HTML*/ `
     <!-- Home feed container -->
     <div id="homefeed" class="feed feed--active">
       <!-- Individual post container -->
-      <div class="post">
-        <!-- Post description section -->
-        <div class="post__description">
-          <!-- User information section -->
-          <div class="post__user">
-            <img
-              class="post__user-profile-img"
-              src="../assets/home/user-icon-profile.svg"
-              alt="avatar"
-            />
-            <a class="post__username-link" href="#/gamerhafsah26">
-              <span class="post__username">
-                gamerhafsah26
-              </span>
-            </a>
-          </div>
-          <!-- Post content section -->
-          <div class="post__content">
-            <p class="post__content-description">
-              porttitor viverra et, dapibus sit amet hulla.porttitor viverra
-              et, dapibus sit amet hulla.porttitor viverra et, dapibus sit
-              amet hulla.porttitor viverra et, dapibus sit amet
-              hulla.porttitor viverra et, dapibus sit amet hulla. porttitor
-              viverra et, dapibus sit amet hulla.porttitor viverra et,
-              dapibus sit amet hulla.porttitor viverra et, dapibus sit amet
-              hulla.porttitor viverra et, dapibus sit amet hulla.porttitor
-              viverra et, dapibus sit amet hulla. porttitor viverra et,
-              dapibus sit amet hulla.porttitor viverra et, dapibus sit amet
-              hulla.porttitor viverra et, dapibus sit amet hulla.porttitor
-              viverra et, dapibus sit amet hulla.porttitor viverra et,
-              dapibus sit amet hulla. porttitor viverra et, dapibus sit amet
-              hulla.porttitor viverra et, dapibus sit amet hulla.porttitor
-              viverra et, dapibus sit amet hulla.porttitor viverra et,
-              dapibus sit amet
-            </p>
-            <img
-              class="post__content-image"
-              src="../assets/post/post1-example.png"
-              alt="attached img"
-            />
-          </div>
-          <!-- Post details section -->
-          <div class="post__details">
-            <!-- Post interactions section -->
-            <div class="post__interactions">
-              <button id="svgButton" class="post__button-like">
-                <img
-                  id="svgImage"
-                  src="../assets/home/normal-thumb.svg"
-                  alt="like button"
-                />
-              </button>
-              <span class="post__button-like-count">15</span>
-              <img
-                class="post__button-comment"
-                src="../assets/home/comment.svg"
-                alt="comment button"
-              />
-              <span class="post__button-comment-count">2</span>
-            </div>
-            <!-- Post time section -->
-            <time class="post__time" datetime="2023-10-01">10/01/2023</time>
-          </div>
-        </div>
-        <div class="post__comment-section">
-          <!-- Existing comments will be dynamically inserted here -->
-          <div class="post__comment">
-            <div class="post__comment-header">
-              <img
-              src="../assets/home/user-icon-profile.svg"
-              alt="user icon"
-              class="post__comment-user-icon"
-              />
-              <a class="post__author-link" href="#/John-Doe">
-                <span class="post__comment-author">
-                  John Doe
-                </span>
-              </a>
-            </div>
-            <div class="post__comment-body">This is a comment.</div>
-            <span class="post__comment-date">2022-01-01</span>
-          </div>
-          <div class="post__comment">
-            <div class="post__comment-header">
-              <img
-                src="../assets/home/user-icon-profile.svg"
-                alt="user icon"
-                class="post__comment-user-icon"
-              />
-              <a class="post__author-link" href="#/John-Doe">
-                <span class="post__comment-author">
-                  John Doe
-                </span>
-              </a>
-            </div>
-            <div class="post__comment-body">
-              This is a comment.
-              This is a comment.
-              This is a comment.
-              This is a comment.
-              This is a comment.
-              This is a comment.
-              This is a comment.
-              This is a comment.
-              This is a comment.
-              This is a comment.
-              This is a comment.
-              This is a comment.
-              This is a comment.
-              This is a comment.
-              This is a comment.
-              This is a comment.
-              This is a comment.
-              This is a comment.
-              This is a comment.
-              This is a comment.
-              This is a comment.
-              This is a comment.
-              This is a comment.
-              This is a comment.
-            </div>
-            <span class="post__comment-date">2022-01-01</span>
-          </div>
-          <div class="post__comment">
-            <div class="post__comment-header">
-              <img
-              src="../assets/home/user-icon-profile.svg"
-              alt="user icon"
-              class="post__comment-user-icon"
-              />
-              <a class="post__author-link" href="#/John-Doe">
-                <span class="post__comment-author">
-                  John Doe
-                </span>
-              </a>
-            </div>
-            <div class="post__comment-body">This is a comment.</div>
-            <span class="post__comment-date">2022-01-01</span>
-          </div>
-        </div>
-        <div class="post__add-comment">
-          <div class="post__add-comment-header">
-            <img
-            src="../assets/home/user-icon-profile.svg"
-            alt="user icon"
-            class="post__comment-user-icon"
-            />
-            <span class="post__comment-user-text">You</span>
-          </div>
-          <input
-            type="text"
-            class="post__add-comment-input"
-            placeholder="Add a comment..."
-          />
-          <button class="post__add-comment-submit">Post</button>
-        </div>
-      </div>
     </div>
     <div id="newsfeed" class="feed feed--inactive">
     </div>
   </div>
   `;
+
+/**
+ * Directs the user to the profile page when a name link is clicked.
+ */
+function directToProfile() {
+  const linkUser = document.querySelectorAll(".name-link");
+
+  linkUser.forEach((link) => {
+    link.addEventListener("click", async (e) => {
+      e.preventDefault();
+      const username = link.querySelector(".name-value").innerText;
+
+      const res = await fetch(`/getProfile?username=${username}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (res.status === 200) {
+        const data = await res.json();
+        const isSessionUser = data.isSessionUser;
+
+        if (isSessionUser) {
+          window.location.hash = "#/profile";
+        } else {
+          window.location.hash = `#/userProfile/${username}`;
+        }
+      }
+    });
+  });
+}
+
+function addComment() {
+  const addCommentButton = document.querySelectorAll(".post__add-comment-submit");
+
+  addCommentButton.forEach((button) => {
+    button.addEventListener("click", async () => {
+      const commentInput = button.previousElementSibling;
+      const comment = commentInput.value;
+      const postId = button.parentElement.parentElement.id;
+
+      console.log("postId: ", postId);
+      console.log("comment: ", comment);
+
+      const today = new Date();
+      const date = today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+
+      if (comment) {
+        const res = await fetch("/add-comment", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ commentText: comment, commentDate: date, postId: postId }),
+        });
+
+        const resData = await res.json();
+
+        if (res.status === 200) {
+          const commentSection = button.parentElement.previousElementSibling;
+          const commentElement = document.createElement("div");
+          commentElement.className = "post__comment";
+          commentElement.innerHTML = /*html*/ `
+          <div class="post__comment-header">
+            <img
+            src="../assets/home/user-icon-profile.svg"
+            alt="user icon"
+            class="post__comment-user-icon"
+            />
+            <a class="post__author-link name-link">
+              <span class="post__comment-author name-value">
+                ${resData.username}
+              </span>
+            </a>
+          </div>
+          <div class="post__comment-body">${comment}</div>
+          <span class="post__comment-date">${date}</span>
+          `;
+          commentSection.appendChild(commentElement);
+          commentInput.value = "";
+        }
+      }
+    });
+  });
+}
 
 /**
  * Initializes the home page functionality.
@@ -240,6 +169,10 @@ function initHome() {
       isClicked = true;
     }
   });
+
+  directToProfile();
+
+  addComment();
 
   toggleButtons.forEach((button) => {
     button.addEventListener("click", () => {
@@ -283,23 +216,19 @@ function initHome() {
   });
 }
 
-async function getHomeFeed() {
-  const res = await fetch("/homefeed", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-  const data = await res.json();
-
-  console.log("Checking response");
-
-  if (res.status === 200) {
-    data.forEach((post) => {
-      const postElement = document.createElement("div");
-      postElement.classList.add("post");
-      postElement.id = post._id;
-      postElement.innerHTML = /*html*/ `
+/**
+ * Appends posts to the feed container.
+ *
+ * @param {string} container - The ID of the container element where the posts will be appended.
+ * @param {Array} posts - An array of post objects to be appended to the feed.
+ * @returns {void}
+ */
+function appendPostToFeed(container, posts) {
+  posts.forEach((post) => {
+    const postElement = document.createElement("div");
+    postElement.classList.add("post");
+    postElement.id = post._id;
+    postElement.innerHTML = /*html*/ `
       <div class="post__description">
           <!-- User information section -->
           <div class="post__user">
@@ -308,8 +237,8 @@ async function getHomeFeed() {
               src="../assets/home/user-icon-profile.svg"
               alt="avatar"
             />
-            <a class="post__username-link" href="#/gamerhafsah26">
-              <span class="post__username">
+            <a class="post__username-link name-link">
+              <span class="post__username name-value">
                 ${post.username}
               </span>
             </a>
@@ -319,7 +248,7 @@ async function getHomeFeed() {
             <p class="post__content-description">
               ${post.postDescription}
             </p>
-            <!--TODO Load image with javascript -->
+            <!-- TODO Load image with javascript -->
             <!-- <img
               class="post__content-image"
               src="${post.image}"
@@ -350,11 +279,79 @@ async function getHomeFeed() {
           </div>
         </div>
       `;
-      document.getElementById("homefeed").appendChild(postElement);
+
+    const commentSection = document.createElement("div");
+    commentSection.className = "post__comment-section";
+
+    post.comment_list.forEach((comment) => {
+      const commentElement = document.createElement("div");
+      commentElement.className = "post__comment";
+      commentElement.innerHTML = /*html*/ `
+        <div class="post__comment-header">
+          <img
+          src="../assets/home/user-icon-profile.svg"
+          alt="user icon"
+          class="post__comment-user-icon"
+          />
+          <a class="post__author-link name-link" href="#/John-Doe">
+            <span class="post__comment-author name-value">
+              ${comment.username}
+            </span>
+          </a>
+        </div>
+        <div class="post__comment-body">${comment.commentText}</div>
+        <span class="post__comment-date">${comment.commentDate}</span>
+        `;
+      commentSection.appendChild(commentElement);
     });
 
+    postElement.innerHTML += commentSection.outerHTML;
+
+    const addCommentSection = document.createElement("div");
+    addCommentSection.className = "post__add-comment";
+
+    addCommentSection.innerHTML = /*html*/ `
+      <div class="post__add-comment-header">
+        <img
+        src="../assets/home/user-icon-profile.svg"
+        alt="user icon"
+        class="post__comment-user-icon"
+        />
+        <span class="post__comment-user-text">You</span>
+      </div>
+      <input
+        type="text"
+        class="post__add-comment-input"
+        placeholder="Add a comment..."
+      />
+      <button class="post__add-comment-submit">Post</button>
+      `;
+
+    postElement.innerHTML += addCommentSection.outerHTML;
+
+    document.getElementById(`${container}`).appendChild(postElement);
+  });
+}
+
+/**
+ * Fetches the home feed data from the server and appends it to the feed.
+ * Initializes the home view.
+ * @returns {Promise<void>} A promise that resolves when the home feed data is fetched and appended to the feed.
+ */
+async function getHomeFeed() {
+  const res = await fetch("/homefeed", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = await res.json();
+
+  if (res.status === 200) {
+    appendPostToFeed("homefeed", data);
     initHome();
   }
 }
 
-export { loadHomeFeedPage, getHomeFeed };
+export { loadHomeFeedPage, getHomeFeed, appendPostToFeed, addComment, directToProfile };
